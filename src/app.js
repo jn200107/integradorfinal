5aa01107101505d1f6e64da924b5a7bca94c4a3f
jorge navarro <jn200107@gmail.com>
2023-01-11 11:59:11 -0500

    #1

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import { BD } from './db.js'
+import { PORT } from './config.js'
+
+
+const app = express()
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.get('/', async (req, res) => {
+    const rows = await BD.query('SELECT * from peliculas')
+    res.json(rows)
+})
+
+app.get('/crearPelicula', async (req,res) =>{
+    const resultado = await BD.query('INSERT INTO peliculas(nombre) VALUES ("Guerra de las galaxias")')
+    res.json(resultado)
+})
+
+app.listen(PORT)
+console.log('Server en puerto:', PORT)
