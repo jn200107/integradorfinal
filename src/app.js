3680fedd446bf26762b0b55943cc6fef983291dd
jorge navarro <jn200107@gmail.com>
2023-04-20 18:21:58 -0500

    cambios

--- src/app.js
+++ src/app.js
@@ -15,15 +15,66 @@ app.use((req, res, next) => {
 });
 
 // creamos nuestras urls 
-app.get('/', async (req, res) => {
-    const rows = await BD.query('SELECT * from peliculas')
-    res.json(rows)
+
+app.get('/solicitud',(req,res)=>{
+    let mysql='SELECT * FROM solicitudes'
+
+    BD.query(mysql,(err,resuls)=>{
+        if(err) throw err
+        if (resuls.length>0){
+            res.json(resuls)
+        } else{
+            res.send('no hay datos disponibles')
+        }
+    })
+
 })
 
-app.get('/crearPelicula', async (req,res) =>{
-    const resultado = await BD.query('INSERT INTO peliculas(nombre) VALUES ("Guerra de las galaxias")')
-    res.json(resultado)
+
+
+app.post('/nueva-solicitud',(req,res)=>{
+    const mysql= 'INSERT INTO solicitudes SET ?'
+
+    const solicitudOBJ={
+        idsolicitudes : req.body.idsolicitudes,
+        nombre : req.body.nombre,
+        correo: req.body.correo,
+        telefono: req.body.telefono,
+        solicitud: req.body.solicitud,
+        comentario: req.body.comentario,
+    }
+
+    BD.query(mysql,solicitudOBJ,err=>{
+        if (err) throw err
+
+        res.send('solicitud añadida con exito')
+    })
 })
+
+app.put('/actualizar-solicitud/:id',(req,res)=>{
+    const id=req.params
+    const {nombre,correo,telefono,solicitud,comentario}= req.body
+
+    const mysql= `UPDATE solicitudes SET nombre ="${nombre}", correo="${correo}", telefono="${telefono}", solicitud="${solicitud}",comentario="${comentario}" where idsolicitudes = ${id.id}`
+    
+    BD.query(mysql, err =>{
+        if (err) throw err
+
+        res.send('solicitud actualizada')
+    })
+})
+
+app.delete('/eliminar-solicitud/:id',(req,res)=>{
+    const id= req.params
+    const mysql =`DELETE FROM solicitudes where idsolicitudes = ${id.id} `
+    
+    BD.query(mysql,err=>{
+        if (err) throw err
+
+        res.send ('solicitud eliminada')
+    })
+})
+
 
 app.listen(PORT)
-console.log('Server en puerto:', PORT)
+console.log('app en puerto:', PORT)
